e90acdc126e5aa1dfc86b58d203d1696e80be28e
tadp <june@tutormentor.org>
2013-08-21 09:56:43 -0700

    Create model-backed form for new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,15 +5,26 @@ def index
 end
 
 def show
-  @users=User.all
+  @post = Post.find(params[:id])
 end
 
 
 def new
-  @users=User.all
+  @post = Post.new
+
 end
 
 def create
+  post = Post.new(params.require(:post).permit!)
+
+  if post.save
+    flash[:notice] = "You created a new post!"
+    # redirect_to posts_path, notice: "Another syntax"
+    redirect_to posts_path
+  else
+    #handle validations
+    render
+  end
 end
 
 def edit
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :creator, class_name: 'User', foreign_key: :user_id
   has_many :comments
   has_many :post_categories
   has_many :categories, through: :post_categories
