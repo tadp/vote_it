43cc1a662eda44098b01c386aa74f9ce722d1e74
tadp <june@tutormentor.org>
2013-08-20 18:04:37 -0700

    Setting up the posts_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,27 @@
 class PostsController < ApplicationController
-  def index
-  	@post=Post.all
 
- def show
- end
+def index
+	@posts=Post.all
+end
+
+def show
+  @users=User.all
+end
 
 
-  def new
-  end
+def new
+  @users=User.all
+end
 
-  def create
-  end
+def create
+end
 
-  def edit
-  	@user= params[:id]
-  end
+def edit
+	@user= params[:id]
+end
 
-  def update
-  end
+def update
+end
 
 end
 
