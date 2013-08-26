6c5d1e3da371ceac161e3dd4acfd337c0ca21cc5
tadp <june@tutormentor.org>
2013-08-26 10:40:49 -0700

    Deletes bad migrations

--- db/migrate/20130817132540_change_categories.rb
+++
@@ -1,7 +0,0 @@
-class ChangeCategories < ActiveRecord::Migration
-  def change
-  	change_table :categories do |t|
-  		t.remove :post_id
-  	end
-  end
-end
--- db/migrate/20130817170515_drop_table_post_categories.rb
+++
@@ -1,5 +0,0 @@
-class DropTablePostCategories < ActiveRecord::Migration
-  def change
-  	drop_table :post_categories
-  end
-end
